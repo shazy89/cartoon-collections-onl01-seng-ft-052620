502e51b8ecf45884cc2932fa93c43ba070d78480
Ed Shaziman <erdogan_oh@hotmail.com>
2020-05-29 17:52:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,6 +16,6 @@ end
 
 def find_the_cheese(cheese_types)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.select {|x| x.include?("ch") }
+  cheese_types.select {|x| x.include?("ch") }
   
 end
