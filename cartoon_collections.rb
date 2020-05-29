9cd9f3760d76a17f8d76dbc3d71f6b465083d230
Ed Shaziman <erdogan_oh@hotmail.com>
2020-05-29 18:25:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,7 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
- 
+ cheese_types = ["cheddar", "gouda", "camembert"]
 result = array.find {|cheese| cheese_types.include?(cheese) }
 result.nil? ? nil : result
 end
