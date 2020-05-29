ffe608ca25ab4e3f83263028d4e0f595d9839d0d
Ed Shaziman <erdogan_oh@hotmail.com>
2020-05-29 17:11:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,11 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(name)
+  name.each_with_index {|element , index| puts "#{index + 1} #{element}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+def summon_captain_planet(array)
+  array.map {|planet| puts planet.upcase + "!"}
+  
 end
 
 def long_planeteer_calls# code an argument here
