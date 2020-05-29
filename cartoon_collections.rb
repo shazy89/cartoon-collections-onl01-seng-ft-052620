ee52b2c0c806198de3828aec0878de452560760d
Ed Shaziman <erdogan_oh@hotmail.com>
2020-05-29 17:53:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,7 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(cheese_types)
-  cheese_types = ["cheddar", "gouda", "camembert"]
+  
   cheese_types.select {|x| x.include?("ch") }
   
 end
